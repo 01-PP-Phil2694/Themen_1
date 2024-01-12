ed2c6b076ffb0586fcfb424e953d6709b5d97add
Phil2694 <155629796+Phil2694@users.noreply.github.com>
2024-01-12 09:14:39 +0000

    08Codeblöcke.Kapseln

---
+++ src/08_funktionen_I.js
@@ -0,0 +1,14 @@
+
+/***** Funktionen 01*****/
+// 1. Kapselung von Codeblöcken
+
+// Funktionsaufruf / call
+test();
+
+
+// Funktionsrumpf / Funktionsdeklaration (callee)
+function test()
+{
+    console.log("Hallo, Benjamin!");
+}
+
