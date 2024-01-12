210b0c223c465543a3650bc55bc5b96f78349ba2
Phil2694 <155629796+Phil2694@users.noreply.github.com>
2024-01-12 10:52:48 +0000

    08-2cParams

--- src/08_funktionen_I.js
+++ src/08_funktionen_I.js
@@ -1,14 +1,60 @@
 
 /***** Funktionen 01*****/
-// 1. Kapselung von Codeblöcken
+/// 1. Kapselung von Codeblöcken
 
-// Funktionsaufruf / call
-test();
+/// Funktionsaufruf / call
+//test();
 
 
-// Funktionsrumpf / Funktionsdeklaration (callee)
-function test()
+/// Funktionsrumpf / Funktionsdeklaration (callee)
+//function test()
+//{
+//    console.log("Hallo, Felix!");
+//}
+
+
+/***** Funktionen 02a *****/
+/// 2a. Parametisierung + Datenübergabe von INNEN
+
+/// Call
+//ausgabeNamen();
+
+/// Funktion
+function ausgabeNamen() 
 {
-    console.log("Hallo, Benjamin!");
+    ///interne Variable
+    let firstName = "Felix";
+    console.log("Hallo, " + firstName + "!");
 }
+
+///console.log(firstName); = Fehler: Scope!
+
+
+/***** Funktionen 02b *****/
+/// 2b. Parametrisierung + Datenübergabe von AUSSEN
+
+/// Argumente sind Daten für Parameter
+//ausgabeNamen2("Felix");  /// Argument
+//ausgabeNamen2("Denise");
+//ausgabeNamen2("Kurt");
+
+function ausgabeNamen2(firstName)  /// Parameter
+{
+    console.log("Hallo, " + firstName + "!");
+}
+
+
+/***** Funktion 02c *****/
+/// 2c. Mehrere Parameter / Argumente
+
+//ausgabeNamenParams("Max","Mütze");
+
+const prompt = require('prompt-sync')({sigint: true});
+ausgabeNamenParams(prompt("Bitte Vornamen eingeben:"),prompt("Ihren Nachnamen bitte eingeben:"));  /// Piping
+
+function ausgabeNamenParams(firstName, familyName) /// Parameter 
+{
+    console.log("Hallo, " + firstName + " " + familyName + "!");
+}
+
 
