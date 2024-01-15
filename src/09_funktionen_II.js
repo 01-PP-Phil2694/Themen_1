72152630db17c9286db1633b417e9b76bf37bb4a
Phil2694 <155629796+Phil2694@users.noreply.github.com>
2024-01-15 11:08:56 +0000

    09RechnerRechenart

--- src/09_funktionen_II.js
+++ src/09_funktionen_II.js
@@ -5,17 +5,45 @@
 0. a+b | a-b | a*b | a/b  --> Ergebnis c 
 
 1. Dateneingabe + -überprüfung :  
-2. Auswahl Rechenart : 
-3. Fkt. Grundrechenarten : 
+2. Auswahl Rechenart : check!
+3. Fkt. Grundrechenarten : check!
 4. Ausgabe in Konsole : check!
 */
 
 
 const ERROR_STR_DIV = "Division durch 0 ist nicht möglich!";
+const ERROR_STR_GEN = "Irgendwas ging schief!";
+// module: calculator | tests:
+// agreement : "+","-","*",":","/"
+output(calculator(3,2,"+"));
+output(calculator(3,2,"-"));
+output(calculator(3,2,"*"));
+output(calculator(3,2,":"));
+output(calculator(3,2,"/"));
+output(calculator(3,0,"/"));
+output(calculator(3,2,"#?!"));
+
+function calculator(a,b,op) 
+{
+	switch (op) 
+	{
+		case "+":   ///add()
+			return add(a,b);
+		case "-":	///sub()
+		    return subtract(a,b);
+		case "*":	///mul()
+		    return multiply(a,b);
+		case ":":   ///div()
+		case "/":	///div()
+		    return divide(a,b);
+		default:    /// Error
+		    return ERROR_STR_GEN;
+	}
+}
 
 /// module: divison a / b // test:
-output(divide(4,2));
-output(divide(3,0));
+//output(divide(4,2));
+//output(divide(3,0));
 
 function divide(a,b) 
 {
