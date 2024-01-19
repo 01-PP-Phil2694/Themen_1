808876ad92919f2c30ae9c2c470d4abcc2dcd92f
Phil2694 <155629796+Phil2694@users.noreply.github.com>
2024-01-19 09:21:53 +0000

    14objectsFertig

--- src/14_objects.js
+++ src/14_objects.js
@@ -39,20 +39,26 @@ let person =
               salary: [120000, 160000],
               permission: true,
               sayHello: function(){
-                                     return "Hello";
+                                    // return "Hallo, ich bin " + person.firstName + " " + person.familyName;
+                                     return "Hallo, ich bin " + this.firstName + " " + this.familyName;
                                   }
             };
 
 //output(person);
-output(person.familyName);
-output(person.firstName);
-output(person.salary[1]);
-output(person.permission);
-output(person.sayHello());
+//output(person.familyName);
+//output(person.firstName);
+//output(person.salary[1]);
+//output(person.permission);
+//output(person.sayHello());
 
-const txt = person.sayHello() + ", ich bin " + person.firstName + 
-            " und verdiene " + person.salary[1] + "p.a"
+//const txt = person.sayHello() + ", ich bin " + person.firstName + 
+//            " und verdiene " + person.salary[1] + "p.a"
+
+const txt = person.sayHello() + " und verdiene " + person.salary[1] + "p.a"
+
 output(txt);
+
+
 
 // Modul: Ausgabe | Test
 //output("hi");
